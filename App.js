6a2785f618eca42f36ee1ba4360ce2c4d5a397c0
i330z <izzazahmed134@gmail.com>
2019-09-25 17:49:26 +0530

    adding push notification

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
-
-
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+import Expo from 'expo';
 
 import LoginScreen from './screen/LoginScreen'
 import LoadingScreen from './screen/LoadingScreen'
@@ -13,10 +14,34 @@ import * as firebase from 'firebase';
 import { firebaseConfig } from './config' 
 firebase.initializeApp(firebaseConfig);
 
+// export default class App extends Component {
+//   render(){
+//     return (
+//       <AppNavigator />
+//     );
+//   }
+// }
+
+async function register(){
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  console.log(existingStatus)
+  // let token = await Notifications.getExpoPushTokenAsync();
+  // console.log(token)
+}
+
 export default class App extends Component {
+
+  componentWillMount(){
+    register()
+  }
+
   render(){
     return (
-      <AppNavigator />
+      <View>
+        <Text>hello</Text>
+      </View>
     );
   }
 }
--- screen/DashboardScreen.js
+++ screen/DashboardScreen.js
@@ -6,8 +6,48 @@ import {
     Button
 } from "react-native";
 import firebase from 'firebase';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 export default class DashboardScreen extends Component{
+
+    registerForPushNotificationsAsync = async() => {
+        const { status: existingStatus } = await Permissions.getAsync(
+          Permissions.NOTIFICATIONS
+        );
+        let finalStatus = existingStatus;
+      
+        // only ask if permissions have not already been determined, because
+        // iOS won't necessarily prompt the user a second time.
+        if (existingStatus !== 'granted') {
+          // Android remote notification permissions are granted during the app
+          // install, so this will only ask on iOS
+          const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+          finalStatus = status;
+        }
+      
+        // Stop here if the user did not grant permissions
+        if (finalStatus !== 'granted') {
+          return;
+        }
+      
+        try{
+        // Get the token that uniquely identifies this device
+        let token = await Notifications.getExpoPushTokenAsync();
+        console.log(token);
+        // POST the token to your backend server from where you can retrieve it to send push notifications.
+        firebase.database().ref('users/'+ this.currentUser.uid+'/push_token').set(token)
+        }
+        catch(error){
+            console.log(error)
+        }
+      };
+
+    async componentDidMount(){
+        this.currentUser = await firebase.auth().currentUser;
+        await this.registerForPushNotificationsAsync();
+    }
+
     render(){
         return(
             <View style={styles.container}>
